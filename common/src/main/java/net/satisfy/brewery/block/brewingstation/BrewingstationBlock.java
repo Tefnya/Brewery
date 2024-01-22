2859cf47f409c01c0d7fc6c6a14a4dc5f5c48d17
Eduardo Carrascon <carrascon.eduardo@gmail.com>
2024-01-22 12:25:59 +0100

    Cleaner code

--- common/src/main/java/net/satisfy/brewery/block/brewingstation/BrewingstationBlock.java
+++ common/src/main/java/net/satisfy/brewery/block/brewingstation/BrewingstationBlock.java
@@ -1,9 +1,5 @@
 package net.satisfy.brewery.block.brewingstation;
 
-import net.satisfy.brewery.Brewery;
-import net.satisfy.brewery.entity.BrewstationBlockEntity;
-import net.satisfy.brewery.block.property.BrewMaterial;
-import net.satisfy.brewery.registry.BlockStateRegistry;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
@@ -16,64 +12,59 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.satisfy.brewery.Brewery;
+import net.satisfy.brewery.block.property.BrewMaterial;
+import net.satisfy.brewery.entity.BrewstationBlockEntity;
+import net.satisfy.brewery.registry.BlockStateRegistry;
 import org.jetbrains.annotations.Nullable;
 
 public class BrewingstationBlock extends HorizontalDirectionalBlock {
-    public static final EnumProperty<BrewMaterial> MATERIAL;
+    public static final EnumProperty<BrewMaterial> MATERIAL = BlockStateRegistry.MATERIAL;
+
     public BrewingstationBlock(Properties properties) {
         super(properties);
         this.registerDefaultState(this.defaultBlockState());
     }
 
     @Override
-    public BlockState getStateForPlacement(BlockPlaceContext blockPlaceContext) {
-        return this.defaultBlockState().setValue(FACING, blockPlaceContext.getHorizontalDirection().getOpposite());
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
     }
 
     @Override
-    public ItemStack getCloneItemStack(BlockGetter blockGetter, BlockPos blockPos, BlockState blockState) {
-        if(!(this.getClass() == BrewKettleBlock.class)) {
-
-
-            BrewstationBlockEntity blockEntity = getController(blockPos, blockGetter);
-            if(blockEntity != null){
+    public ItemStack getCloneItemStack(BlockGetter getter, BlockPos pos, BlockState state) {
+        if (this.getClass() != BrewKettleBlock.class) {
+            BrewstationBlockEntity blockEntity = getController(pos, getter);
+            if (blockEntity != null) {
                 Brewery.LOGGER.error("Is null (blockEntity)");
-                return blockEntity.getBlockState().getBlock().getCloneItemStack(blockGetter, blockPos, blockState);
+                return blockEntity.getBlockState().getBlock().getCloneItemStack(getter, pos, state);
             }
-
         }
-
-        return super.getCloneItemStack(blockGetter, blockPos, blockState);
+        return super.getCloneItemStack(getter, pos, state);
     }
 
     @Override
-    public void playerWillDestroy(Level level, BlockPos blockPos, BlockState blockState, Player player) {
-        BrewstationBlockEntity brewstationEntity = getController(blockPos, level);
+    public void playerWillDestroy(Level level, BlockPos pos, BlockState state, Player player) {
+        BrewstationBlockEntity brewstationEntity = getController(pos, level);
         if (brewstationEntity != null) {
-            for (BlockPos pos : brewstationEntity.getComponents()) {
-                if (!pos.equals(blockPos)) {
-                    level.removeBlock(pos, false);
-                }
-            }
+            brewstationEntity.getComponents().stream()
+                    .filter(componentPos -> !componentPos.equals(pos))
+                    .forEach(componentPos -> level.removeBlock(componentPos, false));
         }
-        super.playerWillDestroy(level, blockPos, blockState, player);
+        super.playerWillDestroy(level, pos, state, player);
     }
 
     @Nullable
     protected BrewstationBlockEntity getController(BlockPos centerPos, Level level) {
-        for (int x = -1; x <= 1; x++) {
-            for (int y = -1; y <= 1; y++) {
-                BlockEntity blockEntity = level.getBlockEntity(centerPos.offset(x, 0, y));
-                if (blockEntity instanceof BrewstationBlockEntity brewstationEntity && brewstationEntity.isPartOf(centerPos)) {
-                    return brewstationEntity;
-                }
-            }
-        }
-        return null;
+        return findController(centerPos, level);
     }
 
     @Nullable
-    protected BrewstationBlockEntity getController(BlockPos centerPos, BlockGetter blockGetter) {
+    protected BrewstationBlockEntity getController(BlockPos centerPos, BlockGetter getter) {
+        return findController(centerPos, getter);
+    }
+
+    private <T extends BlockGetter> BrewstationBlockEntity findController(BlockPos centerPos, T blockGetter) {
         for (int x = -1; x <= 1; x++) {
             for (int y = -1; y <= 1; y++) {
                 BlockEntity blockEntity = blockGetter.getBlockEntity(centerPos.offset(x, 0, y));
@@ -90,9 +81,4 @@ public class BrewingstationBlock extends HorizontalDirectionalBlock {
         super.createBlockStateDefinition(builder);
         builder.add(FACING);
     }
-
-    static {
-        MATERIAL = BlockStateRegistry.MATERIAL;
-    }
-
 }

a7a9149bed1c7b14ff71d07eca094a6c5f3cd9df
Eduardo Carrascon <carrascon.eduardo@gmail.com>
2024-01-23 22:44:08 +0100

    feature(effects): Firework effect when hit enttiy event

--- common/src/main/java/net/satisfy/brewery/event/FunnyParticleSpawnEvent.java
+++ common/src/main/java/net/satisfy/brewery/event/FunnyParticleSpawnEvent.java
@@ -3,7 +3,6 @@ package net.satisfy.brewery.event;
 import dev.architectury.event.EventResult;
 import dev.architectury.event.events.common.PlayerEvent;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.ListTag;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
@@ -16,32 +15,22 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.EntityHitResult;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Random;
-
-public class ParticleSpawnEvent implements PlayerEvent.AttackEntity {
-    private final Random random = new Random();
-
+public class FunnyParticleSpawnEvent implements PlayerEvent.AttackEntity {
     @Override
     public EventResult attack(Player player, Level level, Entity target, InteractionHand hand, @Nullable EntityHitResult result) {
         if (player.hasEffect(MobEffects.DIG_SPEED)) {
-            if (target instanceof LivingEntity entity) {
-                int color = random.nextInt(0xFFFFFF);
-
+            if (target instanceof LivingEntity) {
                 ItemStack fireworkStack = new ItemStack(Items.FIREWORK_ROCKET);
                 CompoundTag fireworkNbt = new CompoundTag();
-                ListTag explosions = new ListTag();
-                CompoundTag explosion = new CompoundTag();
+                fireworkNbt.putInt("Flight", 3);
+                fireworkStack.getOrCreateTagElement("Fireworks").put("Explosions", fireworkNbt);
 
-                explosion.putIntArray("Colors", new int[]{color});
-                explosion.putByte("Type", (byte) 0);
-                explosions.add(explosion);
-                fireworkNbt.put("Explosions", explosions);
-                fireworkNbt.putByte("Flight", (byte) 0);
-                fireworkStack.getOrCreateTagElement("Fireworks").put("Explosions", explosions);
-
-                FireworkRocketEntity fireworkRocket = new FireworkRocketEntity(level, fireworkStack, entity);
-                fireworkRocket.setAirSupply(0);
+                FireworkRocketEntity fireworkRocket = new FireworkRocketEntity(level, fireworkStack, (LivingEntity) target);
                 level.addFreshEntity(fireworkRocket);
+
+                if (!level.isClientSide) {
+                    target.setDeltaMovement(target.getDeltaMovement().add(0, 1.5, 0));
+                }
 
                 return EventResult.pass();
             }

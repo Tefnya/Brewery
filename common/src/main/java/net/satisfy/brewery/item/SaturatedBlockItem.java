1777432b80b1b1034ea5e5b0141cc4ce259af016
satisfyu <104036234+satisfyu@users.noreply.github.com>
2024-02-01 23:45:25 +0100

    Fixed: Few minor issues

--- common/src/main/java/net/satisfy/brewery/item/SaturatedBlockItem.java
+++ common/src/main/java/net/satisfy/brewery/item/SaturatedBlockItem.java
@@ -5,6 +5,7 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import org.jetbrains.annotations.NotNull;
@@ -20,5 +21,8 @@ public class SaturatedBlockItem extends BlockItem {
     @Override
     public void appendHoverText(ItemStack stack, @Nullable Level world, @NotNull List<Component> tooltip, TooltipFlag context) {
         tooltip.add(Component.translatable("tooltip.brewery.stuffed.duration." + this.getDescriptionId()).withStyle(ChatFormatting.BLUE));
+        tooltip.add(Component.empty());
+        tooltip.add(Component.translatable("tooltip.brewery.canbeplaced").withStyle(ChatFormatting.ITALIC, ChatFormatting.GRAY));
+
     }
 }

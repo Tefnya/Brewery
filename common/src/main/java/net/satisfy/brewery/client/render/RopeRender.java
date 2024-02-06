3bfc98ba78f99f9fed7aace2612a2ad1dec687d6
ECarrascon <carrascon.eduardo@gmail.com>
2024-02-06 11:05:55 +0100

    fix(rope): done.

--- common/src/main/java/net/satisfy/brewery/client/render/RopeRender.java
+++ common/src/main/java/net/satisfy/brewery/client/render/RopeRender.java
@@ -17,8 +17,8 @@ public class RopeRender {
     private static final float SCALE = 1.0F;
     private static final float QUALITY = 4.0F;
     private static final int MAX_SEGMENTS = 2048;
-    private static final Vector3f POSITIVE_Y = new Vector3f(0.0F, 1.0F, 0.0F);
-    private static final Vector3f NEGATIVE_Y = new Vector3f(0.0F, -1.0F, 0.0F);
+    private static final Vec3 POSITIVE_Y = new Vec3(0.0F, 1.0F, 0.0F);
+    private static final Vec3 NEGATIVE_Y = new Vec3(0.0F, -1.0F, 0.0F);
     private final Map<Integer, RopeModel> models = new HashMap<>(256);
 
     public void render(final VertexConsumer vertexConsumer, final PoseStack poseStack, final Vec3 ropeVec, final int entityId, final int blockLight0, final int blockLight1, final int skyLight0, final int skyLight1) {
@@ -46,51 +46,52 @@ public class RopeRender {
 
     private void createModel(final RopeModel.Builder builder, final Vec3 ropeVec, final int degrees, final UVCord uv) {
         float length = (float) ropeVec.length();
-        Vector3f ropeNormal = ropeVec.normalize().toVector3f();
-
-        Quaternionf quaternion = new Quaternionf().rotateAxis(degrees, ropeNormal.x(), ropeNormal.y(), ropeNormal.z());
-
-        Vector3f crossVec = ropeNormal.equals(POSITIVE_Y) || ropeNormal.equals(NEGATIVE_Y) ? new Vector3f(1, 0, 0) : new Vector3f(ropeNormal.cross(POSITIVE_Y).normalize());
-
-        crossVec.rotate(quaternion);
-
-        crossVec.mul(((uv.x1() - uv.x0()) / 16.0F) * SCALE);
-        crossVec.mul(0.5F);
+        Vec3 ropeNormal = ropeVec.normalize();
+        // TODO: Everything is working, but the next line (Quaternion quaternion = new Vector3f(ropeNormal).rotationDegrees(degrees); in 1.19.2) I am
+        //  not sure if it is the right port, but again, it works.
+        Quaternionf quaternion = new Quaternionf().rotateAxis((float) degrees, (float) ropeNormal.x(), (float) ropeNormal.y(), (float) ropeNormal.z());
+        Vector3f crossVec = ropeNormal.equals(POSITIVE_Y) || ropeNormal.equals(NEGATIVE_Y) ? new Vector3f(1.0f, 0.0f, 0.0f) : new Vector3f(
+                (float) ropeNormal.cross(POSITIVE_Y).normalize().x,
+                (float) ropeNormal.cross(POSITIVE_Y).normalize().y,
+                (float) ropeNormal.cross(POSITIVE_Y).normalize().z);
+        crossVec.rotate(quaternion); //rotate plane ? degrees
+        crossVec.mul(((uv.x1() - uv.x0()) / 16.0F) * SCALE); //width
+        crossVec.mul(0.5F); //to each side
 
         float uvStart, uvEnd = 0;
         double segmentLength = Math.min(length, 1.0F / QUALITY), actuallySegmentLength;
-        Vector3f currentPos = new Vector3f(), lastPos = new Vector3f();
-        Vector3f segmentVector = new Vector3f(ropeNormal).mul((float) segmentLength);
-        Vector3f segmentPos = new Vector3f();
+        Vector3f currentPos = new Vector3f(0.0f, 0.0f, 0.0f), lastPos = new Vector3f();
+        Vector3f segmentVector = new Vector3f((float) ropeNormal.multiply(segmentLength, segmentLength, segmentLength).x,
+                (float) ropeNormal.multiply(segmentLength, segmentLength, segmentLength).y,
+                (float) ropeNormal.multiply(segmentLength, segmentLength, segmentLength).z), segmentPos = new Vector3f(0.0f, 0.0f, 0.0f);
 
         boolean lastIter = false, straight = (ropeVec.x == 0 && ropeVec.z == 0) || RopeHelper.HANGING_AMOUNT == 0;
         for (int segment = 0; segment < MAX_SEGMENTS; segment++) {
-            lastPos.set(currentPos);
-            segmentPos.add(segmentVector);
+            lastPos.set(currentPos); //current to last
+            segmentPos.add(segmentVector); // add step on top
 
-            if (straight || segmentPos.length() > length) {
+            if (straight || new Vec3(segmentPos).length() > length) {
                 lastIter = true;
                 segmentPos.set((float) ropeVec.x, (float) ropeVec.y, (float) ropeVec.z);
             }
 
-            currentPos.set(segmentPos);
+            currentPos.set(segmentPos); // set currentPos to top
             if (!straight)
-                currentPos.add(0.0F, (float) RopeHelper.getYHanging(segmentPos.length(), ropeVec), 0.0F);
+                currentPos.add(0.0F, (float) RopeHelper.getYHanging(new Vec3(segmentPos).length(), ropeVec), 0.0F); //add hanging
 
-            actuallySegmentLength = new Vector3f(currentPos).distance(lastPos);
+            actuallySegmentLength = new Vec3(currentPos).distanceTo(new Vec3(lastPos));
 
             uvStart = uvEnd;
             uvEnd += (float) (actuallySegmentLength / SCALE);
 
-            builder.vertex(lastPos.x - crossVec.x, lastPos.y - crossVec.y, lastPos.z - crossVec.z).uv(uv.x0() / 16.0F, uvStart).next();
-            builder.vertex(lastPos.x + crossVec.x, lastPos.y + crossVec.y, lastPos.z + crossVec.z).uv(uv.x1() / 16.0F, uvStart).next();
-            builder.vertex(currentPos.x + crossVec.x, currentPos.y + crossVec.y, currentPos.z + crossVec.z).uv(uv.x1() / 16.0F, uvEnd).next();
-            builder.vertex(currentPos.x - crossVec.x, currentPos.y - crossVec.y, currentPos.z - crossVec.z).uv(uv.x0() / 16.0F, uvEnd).next();
+            builder.vertex(lastPos.x() - crossVec.x(), lastPos.y() - crossVec.y(), lastPos.z() - crossVec.z()).uv(uv.x0() / 16.0F, uvStart).next();
+            builder.vertex(lastPos.x() + crossVec.x(), lastPos.y() + crossVec.y(), lastPos.z() + crossVec.z()).uv(uv.x1() / 16.0F, uvStart).next();
+            builder.vertex(currentPos.x() + crossVec.x(), currentPos.y() + crossVec.y(), currentPos.z() + crossVec.z()).uv(uv.x1() / 16.0F, uvEnd).next();
+            builder.vertex(currentPos.x() - crossVec.x(), currentPos.y() - crossVec.y(), currentPos.z() - crossVec.z()).uv(uv.x0() / 16.0F, uvEnd).next();
 
             if (lastIter) break;
         }
     }
-
 
     private int getVectorHash(Vec3 ropeVec, int id) {
         return Objects.hash(id, ropeVec.x, ropeVec.y, ropeVec.z);

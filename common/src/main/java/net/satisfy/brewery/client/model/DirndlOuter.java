95e482161602c07bb9302e6c9b42756f6176e4b7
satisfyu <104036234+satisfyu@users.noreply.github.com>
2024-03-03 14:49:36 +0100

    Fixed: 3d Skinlayer / Armor

--- common/src/main/java/net/satisfy/brewery/client/model/DirndlOuter.java
+++ common/src/main/java/net/satisfy/brewery/client/model/DirndlOuter.java
@@ -23,15 +23,15 @@ public class DirndlOuter<T extends LivingEntity> extends HumanoidModel<T> {
         modelPartData.addOrReplaceChild("hat", CubeListBuilder.create(), PartPose.ZERO);
         modelPartData.addOrReplaceChild("head", CubeListBuilder.create(), PartPose.ZERO);
 
-        modelPartData.addOrReplaceChild("body", CubeListBuilder.create().texOffs(16, 16).addBox(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, new CubeDeformation(0.25F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+        modelPartData.addOrReplaceChild("body", CubeListBuilder.create().texOffs(16, 16).addBox(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, new CubeDeformation(0.35F)), PartPose.offset(0.0F, 0.0F, 0.0F));
 
-        modelPartData.addOrReplaceChild("right_arm", CubeListBuilder.create().texOffs(40, 16).addBox(-3.0F, -2.0F, -2.0F, 4.0F, 10.0F, 4.0F, new CubeDeformation(0.2F)), PartPose.offset(-5.0F, 2.0F, 0.0F));
+        modelPartData.addOrReplaceChild("right_arm", CubeListBuilder.create().texOffs(40, 16).addBox(-3.0F, -2.0F, -2.0F, 4.0F, 10.0F, 4.0F, new CubeDeformation(0.3F)), PartPose.offset(-5.0F, 2.0F, 0.0F));
 
-        modelPartData.addOrReplaceChild("left_arm", CubeListBuilder.create().texOffs(40, 16).addBox(-1.0F, -2.0F, -2.0F, 4.0F, 10.0F, 4.0F, new CubeDeformation(0.2F)), PartPose.offset(5.0F, 2.0F, 0.0F));
+        modelPartData.addOrReplaceChild("left_arm", CubeListBuilder.create().texOffs(40, 16).addBox(-1.0F, -2.0F, -2.0F, 4.0F, 10.0F, 4.0F, new CubeDeformation(0.3F)), PartPose.offset(5.0F, 2.0F, 0.0F));
 
-        modelPartData.addOrReplaceChild("right_leg", CubeListBuilder.create().texOffs(0, 24).addBox(-2.0F, 8.0F, -2.0F, 4.0F, 4.0F, 4.0F, new CubeDeformation(0.25F)), PartPose.offset(-1.9F, 12.0F, 0.0F));
+        modelPartData.addOrReplaceChild("right_leg", CubeListBuilder.create().texOffs(0, 24).addBox(-2.0F, 8.0F, -2.0F, 4.0F, 4.0F, 4.0F, new CubeDeformation(0.35F)), PartPose.offset(-1.9F, 12.0F, 0.0F));
 
-        modelPartData.addOrReplaceChild("left_leg", CubeListBuilder.create().texOffs(0, 24).addBox(-2.0F, 8.0F, -2.0F, 4.0F, 4.0F, 4.0F, new CubeDeformation(0.25F)), PartPose.offset(1.9F, 12.0F, 0.0F));
+        modelPartData.addOrReplaceChild("left_leg", CubeListBuilder.create().texOffs(0, 24).addBox(-2.0F, 8.0F, -2.0F, 4.0F, 4.0F, 4.0F, new CubeDeformation(0.35F)), PartPose.offset(1.9F, 12.0F, 0.0F));
 
         return LayerDefinition.create(modelData, 64, 64);
     }
--- common/src/main/java/net/satisfy/brewery/client/model/LederhosenInner.java
+++ common/src/main/java/net/satisfy/brewery/client/model/LederhosenInner.java
@@ -22,13 +22,13 @@ public class LederhosenInner<T extends LivingEntity> extends HumanoidModel<T> {
 
         ModelPartData.addOrReplaceChild("hat", CubeListBuilder.create(), PartPose.ZERO);
         ModelPartData.addOrReplaceChild("head", CubeListBuilder.create(), PartPose.ZERO);
-        ModelPartData.addOrReplaceChild("body", CubeListBuilder.create().texOffs(16, 32).addBox(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, new CubeDeformation(0.3F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+        ModelPartData.addOrReplaceChild("body", CubeListBuilder.create().texOffs(16, 32).addBox(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, new CubeDeformation(0.4F)), PartPose.offset(0.0F, 0.0F, 0.0F));
         ModelPartData.addOrReplaceChild("right_arm", CubeListBuilder.create(), PartPose.ZERO);
         ModelPartData.addOrReplaceChild("left_arm", CubeListBuilder.create(), PartPose.ZERO);
 
-        ModelPartData.addOrReplaceChild("right_leg", CubeListBuilder.create().texOffs(0, 32).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.26F)), PartPose.offset(-1.9F, 12.0F, 0.0F));
+        ModelPartData.addOrReplaceChild("right_leg", CubeListBuilder.create().texOffs(0, 32).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.36F)), PartPose.offset(-1.9F, 12.0F, 0.0F));
 
-        ModelPartData.addOrReplaceChild("left_leg", CubeListBuilder.create().texOffs(0, 32).mirror().addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.26F)).mirror(false), PartPose.offset(1.9F, 12.0F, 0.0F));
+        ModelPartData.addOrReplaceChild("left_leg", CubeListBuilder.create().texOffs(0, 32).mirror().addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.36F)).mirror(false), PartPose.offset(1.9F, 12.0F, 0.0F));
 
         return LayerDefinition.create(meshdefinition, 64, 64);
     }
